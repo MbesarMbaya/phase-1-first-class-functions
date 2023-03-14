d8405f32217e0aa028ad9ea9ece941abc62594e1
Roy Mbaya <roymbaya08@gmail.com>
2023-03-14 14:36:28 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const receivesAFunction = callBack => callBack();
+function returnsANamedFunction() {
+    return function iHaveAName() {
+      console.log("I have a name!");
+    };
+}
+
+function returnsAnAnonymousFunction(){
+    return () => console.log("I am Anonymous");
+}
